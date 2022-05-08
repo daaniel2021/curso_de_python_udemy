2683bba85d98ae17c0c4859040177e6ea0c74a51
daniel <202109267132@alunos.estacio.br>
2022-05-07 22:43:25 -0300

    Tipos de dados

---
+++ section_02/aula_04/aula04.py
@@ -0,0 +1,19 @@
+"""
+Tipos de dados
+str - String - textos 'Assim' "Assim"
+int - Inteiros - 123456 0 -10 -20
+float - real/ponto flutuante - 10.25 22.50
+boll - booleano/lógico - True/False 10 == 10
+"""
+
+#  String: nome
+print('daniel', type('daniel'))
+
+#  Idade: int
+print(34, type(34))
+
+#  Altura: float
+print(1.85, type(1.85))
+
+#  É Maior de Idade: bool
+print(34 >= 18, type(34 >= 18))
