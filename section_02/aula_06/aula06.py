127bb9c6c47aa6e869be851a2e8fa37c9e2ef1b5
daniel <202109267132@alunos.estacio.br>
2022-05-08 16:27:08 -0300

    Iniciar com letra, pode conter números, separar _, letras minusculas

---
+++ section_02/aula_06/aula06.py
@@ -0,0 +1,20 @@
+'''
+Iniciar com letra, pode conter números, separar _, letras minusculas
+'''
+
+nome ='Daniel'
+idade = 34
+altura = 1.85
+peso = 90
+i_maior = idade > 18
+data_1 = True
+data_atual = 2022
+imc = peso / altura ** 2
+
+"""print('Nome: ', nome)
+print('Idade: ', idade)
+print('Altura: ', altura)
+print('É maior de idade ? : ', i_maior)"""
+
+print(f'{nome}, tem, {idade}, anos e seu imc é : {imc}')
+
