b482e68715f74319b2e75a989daeecb4f0407692
daniel <202109267132@alunos.estacio.br>
2022-05-08 21:05:51 -0300

    Introdução à formatação de Strings

---
+++ section_02/aula_07/aula07.py
@@ -0,0 +1,11 @@
+#  Introdução à formatação de Strings
+
+nome = 'Daniel'
+idade = 34
+altura = 1.85
+peso = 90
+imc = peso / altura ** 2
+
+print(nome, 'tem', idade, 'anos de idade e seu imc é', imc)
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
+print('{} tem {} anos de idade e seu imc é {:.2f}'.format(nome, idade, imc))
