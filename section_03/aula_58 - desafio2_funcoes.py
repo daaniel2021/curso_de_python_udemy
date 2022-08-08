5db4d4d64296296b1c0ce82b9f54f0d0f524b919
Daniel <202109267132@alunos.estacio.br>
2022-08-08 20:58:42 -0300

    sessao 03

---
+++ section_03/aula_58 - desafio2_funcoes.py
@@ -0,0 +1,37 @@
+"""
+1 - Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da
+função2 executada.
+"""
+
+# def func1():
+#     return 'Olá Mundo'
+
+# def func2(funcao):
+#     return funcao()
+
+# print(func1()) 
+
+
+"""
+2 - Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da
+função2 executada. Faça a função1 executar duas funções que recebam um número 
+diferente de argumentos.
+"""
+
+
+# def mestre(funcao, *args, **kwargs):
+#     return funcao(*args, **kwargs)
+
+
+# def oi(nome):
+#     return f'oi, {nome}'
+
+
+# def saudacao(nome, saudacao):
+#     return f'{saudacao} {nome}'
+
+
+# executando = mestre(oi, 'Daniel')
+# executando2 = mestre(saudacao, 'Daniel', saudacao = 'Bom dia!')
+# print(executando)
+# print(executando2)
